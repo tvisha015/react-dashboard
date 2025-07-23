cd718d1721e8a85cdccd0ab4e5c28380477ca99a
Tvisha Sharma <tvisha.edu@gmail.com>
2025-07-23 19:19:10 +0530

    feat: integrate Tailwind CSS and Radix UI components

--- src/app/modules/auth/Login.jsx
+++ src/app/modules/auth/Login.jsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
